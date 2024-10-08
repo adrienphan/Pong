ad573462e0f0c4d650fbcd406f4451cee07edafb
adrienphan <adrienphancaophai@gmail.com>
2024-10-08 16:14:25 +0200

    first commit

---
+++ main.lua
@@ -0,0 +1,22 @@
+-- https://github.com/games50/pong
+--"C:\Program Files\LOVE\love.exe" --console "C:\Users\User\Desktop\Mooc JV Harvard\Pong"
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+function love.draw()
+    love.graphics.printf(
+        'hello pong',
+        0,
+        WINDOW_HEIGHT / 2 - 6,
+        WINDOW_WIDTH,
+        'center'
+    )
+end
